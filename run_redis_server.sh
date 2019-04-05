e638697c2fecd5e1c7e4675229aef329da6705ed
PawelJ-PL <inne.poczta@gmail.com>
2019-04-05 08:47:32 +0200

    Update run_redis_server.sh

--- run_redis_server.sh
+++ run_redis_server.sh
@@ -7,6 +7,6 @@ do
         export pair=`echo $var | cut -d '_' -f3-`
         export param=`echo $pair | awk -F= '{print tolower($1)}' | tr '_' '-'`
         export value=`echo $pair | awk -F= '{print $2}'`
-        COMMAND_PARAMS="$COMMAND_PARAMS --$param $value"
+        COMMAND_PARAMS="$COMMAND_PARAMS '--$param $value'"
 done
 eval "redis-server $COMMAND_PARAMS"
